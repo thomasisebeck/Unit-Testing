e3898f9564dce6be97706dba42119d22914dbc17
thomasisebeck <tisebeck@proton.me>
2024-05-24 13:26:51 +0200

    add for calculator is passing

--- calculator/src/logic/logic.ts
+++ calculator/src/logic/logic.ts
@@ -1,32 +1,56 @@
-import './operations.ts.ts'
+import './operations.ts'
 
 class Calculator {
     constructor() {
     }
 
     public add(first: string, second: string): string {
-        return "";
+        return this.toHex(this.toDec(first) + this.toDec(second));
     }
 
-    public subtract(first: string, second: string) : string {
-        return "";
+    // public subtract(first: string, second: string) : string {
+    //     return "";
+    // }
+    //
+    // public multiply(first: string, second: string) : string {
+    //     return "";
+    // }
+    //
+    // public divide(first: string, second: string) : string {
+    //     return "";
+    // }
+
+    private toHex(dec: number): string {
+        let result = "";
+
+        const hexDigits = "0123456789ABCDEF";
+        while (dec > 0) {
+            const remainder = dec % 16;
+            console.log(remainder);
+            result = hexDigits[remainder] + result;
+            dec = Math.floor(dec / 16);
+        }
+        return result;
     }
 
-    public multiply(first: string, second: string) : string {
-        return "";
+    private toDec(hex: string): number {
+        if (hex === undefined || hex.length === 0)
+            throw "invalid hex number"
+
+        const hexDigits = "0123456789ABCDEF";
+
+        let value = 0;
+
+        for (let i = 0; i < hex.length; i++) {
+            const currentCar = hex[i];
+            const position = hexDigits.indexOf(currentCar);
+            //goes up in multiples of 16 each time
+            //it gets higher up
+            value = 16 * value + position;
+        }
+        return value;
     }
 
-    public divide(first: string, second: string) : string {
-        return "";
-    }
-
-    private toHex(num: number)  : string {
-        return "";
-    }
-
-    private toDec(s: string)  : number {
-        return 0;
-    }
 }
 
 export default Calculator;
