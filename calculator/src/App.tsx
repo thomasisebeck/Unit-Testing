3da85ea526c396e8858ce8dfc26048df5c50e8cc
thomasisebeck <tisebeck@proton.me>
2024-05-24 14:33:14 +0200

    styled calculator

--- calculator/src/App.tsx
+++ calculator/src/App.tsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import s from './App.module.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <div className={s.outer}>
+        <div className={s.container}>
+            <div className={s.screen}>
+                <div>45 X 24</div>
+                <div>1080</div>
+            </div>
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+            <div className={s.body}>
+                <div className={s.tools}>
+                    <img src={'history.svg'}/>
+                    <img src={'backspace.svg'}/>
+                </div>
+                <div className={s.buttons}>
+                    <div>M+</div>
+                    <div>M-</div>
+                    <div>MC</div>
+                    <div>MR</div>
+
+                    <div>A</div>
+                    <div>B</div>
+                    <div>C</div>
+                    <div>clear</div>
+
+                    <div>D</div>
+                    <div>E</div>
+                    <div>F</div>
+                    <div>+</div>
+
+                    <div>7</div>
+                    <div>8</div>
+                    <div>9</div>
+                    <div>x</div>
+
+                    <div>4</div>
+                    <div>5</div>
+                    <div>6</div>
+                    <div>-</div>
+
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                    <div>/</div>
+
+                    <div></div>
+                    <div>0</div>
+                    <div></div>
+                    <div>=</div>
+                </div>
+            </div>
+        </div>
+        </div>
+    )
 }
 
 export default App
